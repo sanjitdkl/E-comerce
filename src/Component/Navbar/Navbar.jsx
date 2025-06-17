51af7ba4fe7722d840973571edc0393e26e1e39e
Sanjit Dhakal <dhakalsanjit6@gmail.com>
2025-06-17 21:15:51 +0545

    Refactor code structure for improved readability and maintainability

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -2,8 +2,41 @@ import React from "react";
 import Logo from "../../assets/Logo.png";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa";
 import Darkmode from "./Darkmode";
-const Navbar = () => {
+const Menu = [
+  { id: 1, name: "Home", link: "/#" },
+  { id: 2, name: "Top Rated", link: "/#service" },
+  { id: 3, name: "Kids Wear", link: "/#" },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
+const Navbar = ({ handleOrderPopup }) => {
   return (
     <div>
       <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
@@ -58,7 +91,43 @@ const Navbar = () => {
       </div>
 
       {/* Lower Navbar */}
-      <div></div>
+      <div data-aos="zoom-in" className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 hover:text-blue-600 duration-200"
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* Simple Dropdown and Links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending Products
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {DropdownLinks.map((data) => (
+                  <li key={data.id}>
+                    <a
+                      href={data.link}
+                      className="inline-block w-full rounded-md p-2 hover:bg-blue-300"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
